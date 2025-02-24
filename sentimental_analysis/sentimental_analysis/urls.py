ead27eb3a09044da3a264299b86885274a99febe
Md Jakaria <jakaria.dcc11@gmail.com>
2025-02-24 14:32:20 -0500

    formatted urls.py

--- sentimental_analysis/sentimental_analysis/urls.py
+++ sentimental_analysis/sentimental_analysis/urls.py
@@ -15,9 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import re_path, include
-
-# from django.conf.urls import url
-
 from django.conf import settings
 from django.conf.urls.static import static
 import realworld.views  # Import views from the realworld app
@@ -27,29 +24,87 @@ from django.urls import path
 urlpatterns = [
     re_path('admin/', admin.site.urls),
     re_path('auth/', include('authapp.urls')),
-
-    re_path('auth/login/', auth_views.LoginView.as_view(), name = 'login'),
-    re_path('auth/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    re_path( r'^$',realworld.views.analysis,name='analysis'),
-    re_path(r'^inputimage',realworld.views.inputimage,name = 'inputimage'),
-    re_path(r'^input',realworld.views.input,name = 'input'),
-    re_path(r'^productanalysis',realworld.views.productanalysis,name = 'product analysis'),
-    re_path(r'^textanalysis',realworld.views.textanalysis,name = 'text analysis'),
-    re_path(r'^audioanalysis',realworld.views.audioanalysis,name = 'audio analysis'),
-    re_path(r'^livespeechanalysis',realworld.views.livespeechanalysis,name = 'live speech analysis'),
-    re_path(r'^fbanalysis',realworld.views.fbanalysis,name = 'fb analysis'),
-    re_path(r'^twitteranalysis',realworld.views.twitteranalysis,name = 'twitter analysis'),
-    re_path(r'^redditanalysis',realworld.views.redditanalysis,name = 'reddit analysis'),
-    re_path(r'^recordAudio', realworld.views.recordaudio, name = 'recordAudio'),
-    re_path(r'^newsanalysis',realworld.views.newsanalysis,name = 'news analysis'),
-    re_path(r'^batch_analysis',realworld.views.batch_analysis,name='batch_text_analysis'),
-    # ... other url patterns ...
-    path('profile/', realworld.views.profile_view, name='profile'),
-    path('settings/', realworld.views.settings_view, name='settings'),
-    path('update_profile/', realworld.views.update_profile, name='update_profile'),
-    path('update_account/', realworld.views.update_account, name='update_account'),
-    path('opt_out/', realworld.views.opt_out, name='opt_out'),
-
+    re_path('auth/login/', auth_views.LoginView.as_view(), name='login'),
+    re_path(
+        'auth/logout/',
+        auth_views.LogoutView.as_view(next_page='login'),
+        name='logout'
+    ),
+    re_path(r'^$', realworld.views.analysis, name='analysis'),
+    re_path(r'^inputimage', realworld.views.inputimage, name='inputimage'),
+    re_path(r'^input', realworld.views.input, name='input'),
+    re_path(
+        r'^productanalysis',
+        realworld.views.productanalysis,
+        name='product analysis'
+    ),
+    re_path(
+        r'^textanalysis',
+        realworld.views.textanalysis,
+        name='text analysis'
+    ),
+    re_path(
+        r'^audioanalysis',
+        realworld.views.audioanalysis,
+        name='audio analysis'
+    ),
+    re_path(
+        r'^livespeechanalysis',
+        realworld.views.livespeechanalysis,
+        name='live speech analysis'
+    ),
+    re_path(
+        r'^fbanalysis',
+        realworld.views.fbanalysis,
+        name='fb analysis'
+    ),
+    re_path(
+        r'^twitteranalysis',
+        realworld.views.twitteranalysis,
+        name='twitter analysis'
+    ),
+    re_path(
+        r'^redditanalysis',
+        realworld.views.redditanalysis,
+        name='reddit analysis'
+    ),
+    re_path(
+        r'^recordAudio',
+        realworld.views.recordaudio,
+        name='recordAudio'
+    ),
+    re_path(
+        r'^newsanalysis',
+        realworld.views.newsanalysis,
+        name='news analysis'
+    ),
+    re_path(
+        r'^batch_analysis',
+        realworld.views.batch_analysis,
+        name='batch_text_analysis'
+    ),
+    path('profile/', 
+        realworld.views.profile_view, 
+        name='profile'
+    ),
+    path('settings/', 
+        realworld.views.settings_view, 
+        name='settings'
+    ),
+    path('update_profile/', 
+        realworld.views.update_profile, 
+        name='update_profile'
+    ),
+    path('update_account/',
+        realworld.views.update_account,
+        name='update_account'
+    ),
+    path('opt_out/',
+        realworld.views.opt_out, 
+        name='opt_out'
+    ),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, 
+                      document_root=settings.MEDIA_ROOT
+                    )
--- testAssignment.py
+++ testAssignment.py
@@ -6,16 +6,14 @@ keys = [
     "key e", "key f", "key g", "key h",
     "key i", "key j", "key k"
     ]
-# put the URL or the Name of the source you want each tester to evaluate in this list
-sources = [
-    1, 2, 3, 4, 5, 6, 7 , 8, 9, 10,
-    11, 12 ,13, 14, 15 ,16 ,17, 18,
-    19, 20
-    ]
+# put the URL or the Name of the source you want each 
+# tester to evaluate in this list
+sources = list(range(1, 21))
 
 number_of_lab_rats = len(keys)
 
-# ajust this variable to detemin how many variables you want each tester to evaluate
+# ajust this variable to detemin how many variables you want
+# each tester to evaluate
 sources_per_rat = 5
 key_sources_pairs = {} 
 for x in range(0,number_of_lab_rats):    
